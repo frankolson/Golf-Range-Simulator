8135d5dfa8d8795ccbaa8e43d31faca12ef6c70d
Will Olson <will.olson11@gmail.com>
2022-10-13 13:00:13 -0700

    Add stance selection UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject ball;
     [SerializeField] GameObject club;
     [SerializeField] TextMeshProUGUI currentClubText;
+    [SerializeField] TextMeshProUGUI stanceAngleText;
+    [SerializeField] GameObject stanceImage;
     [SerializeField] float stanceAngle;
     [SerializeField] float power;
     [SerializeField] Vector2 ballStrikeLocation;
 
     private Club clubScript;
     private Ball ballScript;
+
+    private float originalStanceImageAngle;
     
     void Start()
     {
         ballScript = ball.GetComponent<Ball>();
         SelectClub(club);
+        originalStanceImageAngle = stanceImage.transform.eulerAngles.z;
     }
 
     void Update()
@@ -35,6 +40,21 @@ public class PlayerController : MonoBehaviour
         clubScript = this.club.GetComponent<Club>();
 
         currentClubText.text = $"Current Club:\n{clubScript.nickname}";
+    }
+
+    public void SelectStanceAngle(float angleChange)
+    {
+        stanceAngleText.text = $"Stance Angle:\n{angleChange}";
+        stanceAngle = angleChange;
+        SetStanceImageRotation();
+    }
+
+    void SetStanceImageRotation()
+    {
+        Vector3 rotation = stanceImage.transform.eulerAngles;
+        rotation.z = originalStanceImageAngle + stanceAngle;
+
+        stanceImage.transform.eulerAngles = rotation;
     }
 
     void StrikeBall()
